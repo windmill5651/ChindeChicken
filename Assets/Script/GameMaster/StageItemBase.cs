44072900069aaab5cc246bf44b7d96ce57013ca4
Windmill <sobajima.ryoya.565@gmail.com>
2018-05-19 21:23:54 +0900

    ニワトリ制御機能仮追加

---
+++ Assets/Script/GameMaster/StageItemBase.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// ステージ関連の名前空間です。
+/// </summary>
+namespace Chicken.Game.Stage
+{
+
+    /// <summary>
+    ///  StageItemBase
+    ///  ステージに出現するオブジェクトのベースクラスです。
+    /// 
+    /// Author:Windmill
+    /// </summary>
+    public class StageItemBase : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
