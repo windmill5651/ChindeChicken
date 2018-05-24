bbd08c42e4e3e88ad932e17044217321f14c2bba
Windmill <sobajima.ryoya.565@gmail.com>
2018-05-25 00:27:36 +0900

    コミットするよ

---
+++ Assets/TestAsset/TestScript/DebugTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        Debug.Log( "ログ" );
+        Debug.LogError( "えらー", "エラーログ" );
+        Debug.LogWarning( "わーにんぐ", "ワーニングログ" );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
